27109dbb249d2c1daadd0f36e9ec8a8c5db78552
aaugust <tony@Tony-SB2.localdomain>
2019-09-18 22:47:07 -0500

    Fixes agent teardown script

--- common/common-teardown-services-agent.sh
+++ common/common-teardown-services-agent.sh
@@ -7,19 +7,19 @@ ${COMMON_DIR}/common-kubectl-connect.sh
 # Initialize
 ######################
 
+#Stop and Delete deployment if one is active
+echo "Stop and Delete deployment if one is active"
+#TODO Should dynically discover deployment names via REST API based on agent name
+#for i in deployment-${SCH_AGENT_NAME}*.id; do
+
+#Delete Deployment if any
+${COMMON_DIR}/common-teardown-services-deployment.sh
+
 #Delete and Unregister Control Agent if one is active
 if [[ -f "agent-${SCH_AGENT_NAME}.id" && -s "agent-${SCH_AGENT_NAME}.id" ]]; then
     agent_id="`cat agent-${SCH_AGENT_NAME}.id`"
     echo Agent ID: ${agent_id}
     echo K8S Namespace: ${KUBE_NAMESPACE}
-
-    #Stop and Delete deployment if one is active
-    echo "Stop and Delete deployment if one is active"
-    #TODO Should dynically discover deployment names via REST API based on agent name
-    #for i in deployment-${SCH_AGENT_NAME}*.id; do
-
-    #Delete Deployment if any
-    ${COMMON_DIR}/common-teardown-services-deployment.sh
 
     # Delete agent
     echo "Deactivate and delete agent"
@@ -30,27 +30,27 @@ if [[ -f "agent-${SCH_AGENT_NAME}.id" && -s "agent-${SCH_AGENT_NAME}.id" ]]; the
     curl -s -X DELETE "${SCH_URL}/provisioning/rest/v1/dpmAgent/${agent_id}" --header "Content-Type:application/json" --header "X-Requested-By:SDC" --header "X-SS-REST-CALL:true" --header "X-SS-User-Auth-Token:${SCH_TOKEN}"
     rm -f agent-${SCH_AGENT_NAME}.id
 
-    echo "... Delete K8s Pod"
-    cat ${COMMON_DIR}/control-agent.yaml | envsubst > ${PWD}/_tmp_control-agent.yaml
-    kubectl delete -f ${PWD}/_tmp_control-agent.yaml
-    echo "Deleted control agent"
-
-    # Delete secrets
-    echo "... Delete agent secrets"
-    kubectl delete secret ${SCH_AGENT_NAME}-creds \
-        || { echo 'ERROR: Failed to delete SCH credentials secret in Kubernetes'; }
-    kubectl delete secret ${SCH_AGENT_NAME}-compsecret \
-        || { echo 'ERROR: Failed to delete agent keypair secret in Kubernetes' ; }
-
-    # Delete configMap
-    echo "... Delete agent configmap"
-    kubectl delete configmap ${SCH_AGENT_NAME}-config \
-        || { echo 'ERROR: Failed to create configmap in Kubernetes' ; exit 1; }
-
-
 
 else
     echo "File not found: agent-${SCH_AGENT_NAME}.id"
 fi
+
+echo "... Delete K8s Pod"
+cat ${COMMON_DIR}/control-agent.yaml | envsubst > ${PWD}/_tmp_control-agent.yaml
+kubectl delete -f ${PWD}/_tmp_control-agent.yaml
+echo "Deleted control agent"
+
+# Delete secrets
+echo "... Delete agent secrets"
+kubectl delete secret ${SCH_AGENT_NAME}-creds \
+    || { echo 'ERROR: Failed to delete SCH credentials secret in Kubernetes'; }
+kubectl delete secret ${SCH_AGENT_NAME}-compsecret \
+    || { echo 'ERROR: Failed to delete agent keypair secret in Kubernetes' ; }
+
+# Delete configMap
+echo "... Delete agent configmap"
+kubectl delete configmap ${SCH_AGENT_NAME}-config \
+    || { echo 'ERROR: Failed to create configmap in Kubernetes' ; exit 1; }
+
 
 echo Exiting common-teardown-services-agent.sh on cluster ${KUBE_CLUSTER_NAME}
