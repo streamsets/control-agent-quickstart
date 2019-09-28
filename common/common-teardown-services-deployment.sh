2522d09d0a8406523f3eb757e80c5448e05ab84d
aaugust <tony@Tony-SB2.localdomain>
2019-09-28 12:13:02 -0500

    DPM-6764 Workaround

--- common/common-teardown-services-deployment.sh
+++ common/common-teardown-services-deployment.sh
@@ -10,14 +10,17 @@ echo "Stop and Delete deployment if one is active"
 if [[ -f "deployment-${SCH_DEPLOYMENT_NAME}.id" && -s "deployment-${SCH_DEPLOYMENT_NAME}.id" ]]; then
   echo Deleting deployment
   deployment_id="`cat deployment-${SCH_DEPLOYMENT_NAME}.id`"
+  curl -X POST -d "[ \"${deployment_id}\" ]" "${SCH_URL}/provisioning/rest/v1/deployments/stopDeployments" --header "Content-Type:application/json" --header "X-Requested-By:SDC" --header "X-SS-REST-CALL:true" --header "X-SS-User-Auth-Token:${SCH_TOKEN}" | jq -r 'map(select([])|.status)[]'
   deploymentStatus=$(curl -X POST -d "[ \"${deployment_id}\" ]" "${SCH_URL}/provisioning/rest/v1/deployments/status" --header "Content-Type:application/json" --header "X-Requested-By:SDC" --header "X-SS-REST-CALL:true" --header "X-SS-User-Auth-Token:${SCH_TOKEN}" | jq -r 'map(select([])|.status)[]')
-
   # Wait for deployment to become inactive
   while [[ "${deploymentStatus}" != "INACTIVE" ]]; do
     echo "\nCurrent Deployment Status is \"${deploymentStatus}\". Waiting for it to become inactive"
     sleep 10
+
     #Clear any pending errors
-    deploymentStatus=$(curl -X POST -d "[ \"${deployment_id}\" ]" "${SCH_URL}/provisioning/rest/v1/deployments/acknowledgeErrors" --header "Content-Type:application/json" --header "X-Requested-By:SDC" --header "X-SS-REST-CALL:true" --header "X-SS-User-Auth-Token:${SCH_TOKEN}" | jq -r 'map(select([])|.status)[]')
+    #    ---- Commented out pending resolution of JIRA issues DPM-6764
+    #deploymentStatus=$(curl -X POST -d "[ \"${deployment_id}\" ]" "${SCH_URL}/provisioning/rest/v1/deployments/acknowledgeErrors" --header "Content-Type:application/json" --header "X-Requested-By:SDC" --header "X-SS-REST-CALL:true" --header "X-SS-User-Auth-Token:${SCH_TOKEN}" | jq -r 'map(select([])|.status)[]')
+
     #Refresh status
     deploymentStatus=$(curl -X POST -d "[ \"${deployment_id}\" ]" "${SCH_URL}/provisioning/rest/v1/deployments/status" --header "Content-Type:application/json" --header "X-Requested-By:SDC" --header "X-SS-REST-CALL:true" --header "X-SS-User-Auth-Token:${SCH_TOKEN}" | jq -r 'map(select([])|.status)[]')
   done
