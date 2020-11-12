d87f5c3b2cef072425e92af4783bf146820923fc
Eric Kesseler <eric.kesseler@egodigital.onmicrosoft.com>
2020-11-12 13:36:23 +0100

    use AZURE_SUBSCRIPTION parameter for multiple subscriptions

--- k8s-aks/startup.sh
+++ k8s-aks/startup.sh
@@ -6,6 +6,10 @@ source login.sh
 ######################
 
 if [ "$KUBE_CREATE_CLUSTER" == "1" ]; then
+  if [ -n "${AZURE_SUBSCRIPTION}" ]; then
+    echo ... setting Subscription ${AZURE_SUBSCRIPTION}
+    az account set --subscription "${AZURE_SUBSCRIPTION}"
+  fi
   echo Creating K8s Cluster
   # if set, this will also attempt to run the az aks command to provision a cluster
   # create a resource group
@@ -13,6 +17,7 @@ if [ "$KUBE_CREATE_CLUSTER" == "1" ]; then
   az group create --name "${AZURE_RESOURCE_GROUP}" --location "${KUBE_PROVIDER_GEO}" || { echo 'ERROR: Unable to create resource group' ; exit 1; }
   echo ... creating Cluster
   az aks create --resource-group "${AZURE_RESOURCE_GROUP}" \
+  --subscription "${AZURE_SUBSCRIPTION}" \
   --name "${KUBE_CLUSTER_NAME}" \
   --node-count "${KUBE_NODE_INITIALCOUNT}" \
   --node-vm-size "${KUBE_PROVIDER_MACHINETYPE}" \
